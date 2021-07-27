adcffc9e8f7961eb0ce6f313bbcd41b4930eb18e
sophia <sophia@gmail.com>
2021-07-27 23:50:07 +0800

    update sw

--- src/custom-sw.js
+++ src/custom-sw.js
@@ -11,20 +11,20 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest)
 // app-shell
 workbox.routing.registerRoute('/', new workbox.strategies.StaleWhileRevalidate())
 workbox.routing.registerRoute('/api', new workbox.strategies.NetworkFirst({
-    networkTimeoutSeconds: 10
+  networkTimeoutSeconds: 10
 }))
 workbox.routing.registerRoute(/https?:\/\/+(.*)\.(?:png|svg|jpg|gif|webp)$/, new workbox.strategies.CacheFirst({
-    cacheName: 'images',
-    fetchOptions: {mode:'no-cors'},
-    plugins: [
-      new workbox.cacheableResponse.Plugin({
-        statuses: [200],
-        headers: {'X-Cacheable': 'true'}
-      }),
-      new workbox.expiration.Plugin({
-        maxEntries: 30,
-        maxAgeSeconds: 30 * 24 * 60 *60,
-        purgeOnQuotaError: false,
-      })
-    ],
+  cacheName: 'images',
+  fetchOptions: { mode: 'no-cors' },
+  plugins: [
+    new workbox.cacheableResponse.Plugin({
+      statuses: [200],
+      headers: { 'X-Cacheable': 'true' }
+    }),
+    new workbox.expiration.Plugin({
+      maxEntries: 30,
+      maxAgeSeconds: 30 * 24 * 60 * 60,
+      purgeOnQuotaError: false,
+    })
+  ],
 }))
