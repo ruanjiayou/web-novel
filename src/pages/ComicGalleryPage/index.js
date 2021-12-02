d94db98dcec4732e951143255efe25fa7b23b814
ruanjiayou <1439120442@qq.com>
2021-12-02 22:48:27 +0800

    修改bug

--- src/pages/ComicGalleryPage/index.js
+++ src/pages/ComicGalleryPage/index.js
@@ -14,7 +14,9 @@ const model = createPageModel({
 function View({ self, store, router, params, Navi, }) {
   let imageHost = store.lineLoader.getHostByType('image')
   useEffect(() => {
-    self.GalleryLoader.refresh({ params })
+    if (params.id) {
+      self.GalleryLoader.refresh({ params })
+    }
     return () => {
       self.GalleryLoader.clear()
     }
@@ -26,14 +28,14 @@ function View({ self, store, router, params, Navi, }) {
     }
     return <FullHeight>
       <Navi title={self.GalleryLoader.item.title} router={router} />
-      <FullHeightAuto>
+      <FullHeightAuto style={{ fontSize: 0 }}>
         {self.GalleryLoader.item.images.map(image => (
           <img src={imageHost + image} key={image} style={{ width: '100%' }} />
         ))
         }
       </FullHeightAuto>
       <FullHeightFix>
-        {self.GalleryLoader.item.next && <div onClick={() => { router.replaceView('ComicGallery', { bid: self.GalleryLoader.item.next.bid, id: self.GalleryLoader.item.next.id }) }}>{self.GalleryLoader.item.next.title}</div>}
+        {self.GalleryLoader.item.next && <div style={{ textAlign: 'center', padding: '6px 0' }} onClick={() => { router.replaceView('ComicGallery', { bid: self.GalleryLoader.item.next.bid, id: self.GalleryLoader.item.next.id }) }}>{self.GalleryLoader.item.next.title}</div>}
       </FullHeightFix>
     </FullHeight>
   }}</Observer>
