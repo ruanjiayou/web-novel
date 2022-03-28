517997557455f837d1900626d42493962ceb71b0
max <ruanjiayou@phoenixtv.com>
2022-03-28 18:37:40 +0800

    workbox update

--- src/components/Splash/index.js
+++ src/components/Splash/index.js
@@ -21,7 +21,7 @@ export default function Splash({ children, booting, remainSeconds = 5, can = fal
   })
   return <Observer>{() => (
     <Wrapper style={{ display: booting || local.showSplash ? 'block' : 'none', position: 'fixed', left: 0, top: 0 }}>
-      <img src="/logo.jpg" style={{ position: 'absolute', left: '50%', top: '50%', transform: 'translate(-50%,-50%)', borderRadius: '50%', width: 50 }} alt="" />
+      <img src="/novel/logo.jpg" style={{ position: 'absolute', left: '50%', top: '50%', transform: 'translate(-50%,-50%)', borderRadius: '50%', width: 50 }} alt="" />
       {/* <CloseBtn onClick={() => { if (local.remainSeconds === 0 || can) { local.showSplash = false } }}>{local.remainSeconds === 0 ? 'X' : local.remainSeconds}</CloseBtn> */}
       {children}
     </Wrapper>
---
+++ workbox-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+	globDirectory: 'public/',
+	globPatterns: [
+		'**/*.{html,ico,png,jpg,json,css,js}'
+	],
+	swDest: 'public/sw.js',
+	swSrc: 'sw.js'
+};
