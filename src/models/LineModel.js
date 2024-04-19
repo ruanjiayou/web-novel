5d39602b698575ecc3d15ca05b8d756860257951
阮家友 <1439120442@qq.com>
2024-04-19 17:56:16 +0800

    fixbug

--- src/models/LineModel.js
+++ src/models/LineModel.js
@@ -2,12 +2,12 @@ import { types } from 'mobx-state-tree';
 
 const LineModel = types
   .model('Line', {
-    id: types.string,
     name: types.string,
     desc: types.string,
     enabled: types.boolean,
     type: types.string,
-    host: types.string,
+    env: types.string,
+    url: types.string,
   })
   .views((self) => ({}))
   .actions((self) => ({}));
