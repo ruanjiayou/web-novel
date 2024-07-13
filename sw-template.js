12cb4083a75970eb598134a56c5221cbceaeebdd
max <ruanjiayou123@gmail>
2024-07-13 21:18:24 +0800

    test cache

--- sw-template.js
+++ sw-template.js
@@ -28,22 +28,22 @@ registerRoute(
   }),
 );
 
-// 重要的api缓存(离线显示页面)
-const importanceAPI = ['/v1/public/boot', '/v1/public/channels', '/v1/public/group-tree/book-recommend'];
-registerRoute(
-  ({ request }) => {
-    const u = new URL(request.url);
-    return importanceAPI.includes(u.pathname);
-  },
-  new NetworkFirst({
-    cacheName: 'bootApi',
-    plugins: [
-      new CacheableResponsePlugin({
-        statuses: [200],
-      }),
-    ],
-  }),
-);
+// // 重要的api缓存(离线显示页面)
+// const importanceAPI = ['/v1/public/boot', '/v1/public/channels', '/v1/public/group-tree/book-recommend'];
+// registerRoute(
+//   ({ request }) => {
+//     const u = new URL(request.url);
+//     return importanceAPI.includes(u.pathname);
+//   },
+//   new NetworkFirst({
+//     cacheName: 'bootApi',
+//     plugins: [
+//       new CacheableResponsePlugin({
+//         statuses: [200],
+//       }),
+//     ],
+//   }),
+// );
 
 // 预缓存
 precacheAndRoute(self.__WB_MANIFEST);
@@ -57,32 +57,32 @@ registerRoute(
       new CacheableResponsePlugin({ statuses: [200] }),
       new ExpirationPlugin({
         maxEntries: 100,
-        maxAgeSeconds: 7 * 24 * 60 * 60,
-        purgeOnQuotaError: false
+        maxAgeSeconds: 10 * 60,
+        purgeOnQuotaError: true
       })
     ]
   })
 );
 
-// 可以添加更多的缓存策略，例如缓存 CSS、JavaScript 或图片等
-registerRoute(
-  ({ request }) => request.destination === 'style',
-  new CacheFirst()
-);
+// // 可以添加更多的缓存策略，例如缓存 CSS、JavaScript 或图片等
+// registerRoute(
+//   ({ request }) => request.destination === 'style',
+//   new CacheFirst()
+// );
 
-registerRoute(
-  ({ request }) => request.destination === 'script',
-  new CacheFirst()
-);
+// registerRoute(
+//   ({ request }) => request.destination === 'script',
+//   new CacheFirst()
+// );
 
-// Catch routing errors, like if the user is offline
-setCatchHandler(async ({ event }) => {
-  // Return the precached offline page if a document is being requested
-  if (event.request.destination === 'document') {
-    return matchPrecache('/novel/offline.html');
-  }
-  return Response.error();
-});
+// // Catch routing errors, like if the user is offline
+// setCatchHandler(async ({ event }) => {
+//   // Return the precached offline page if a document is being requested
+//   if (event.request.destination === 'document') {
+//     return matchPrecache('/novel/offline.html');
+//   }
+//   return Response.error();
+// });
 
 // 动态缓存
 // registerRoute(
